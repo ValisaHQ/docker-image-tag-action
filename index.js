c3f11b3c4f3f818b0492b487f31732b096d59304
Siarhei Lipchyk <siarhei@lipchyk.com>
2020-03-23 00:07:03 +0100

    Tiny syntax fixes

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ try {
   const registryBase = core.getInput("docker-registry-base-name");
   const repoName = process.env["GITHUB_REPOSITORY"].split('/')[1];
   const shortSha = process.env["GITHUB_SHA"].slice(0, 8);
-  const sanitizedBranchName = process.env["GITHUB_REF"].split("refs/heads/")[1].replace("/", "-")
-  const dockerImageName = `${registryBase}/${repoName}`
+  const sanitizedBranchName = process.env["GITHUB_REF"].split("refs/heads/")[1].replace("/", "-");
+  const dockerImageName = `${registryBase}/${repoName}`;
 
-  core.setOutput("docker-image-branch", `${dockerImageName}:${sanitizedBranchName}`)
-  core.setOutput("docker-image-branch-sha", `${dockerImageName}:${sanitizedBranchName}-${shortSha}`)
+  core.setOutput("docker-image-branch", `${dockerImageName}:${sanitizedBranchName}`);
+  core.setOutput("docker-image-branch-sha", `${dockerImageName}:${sanitizedBranchName}-${shortSha}`);
 } catch (error) {
   core.setFailed(error.message);
 }
